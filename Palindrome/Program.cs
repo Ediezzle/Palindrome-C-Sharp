66191cef7ed49ce8eda1ba32c321af3ac1a5ca15
Ediezzle <edmore.mandikiyana@gmail.com>
2020-01-27 15:05:27 +0200

    Add project files.

---
+++ Palindrome/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        public static bool IsPalindrome(string word)
+        {
+            //declare an empty string           
+            string myString = "";
+
+            //iterate through all the characters in the string we need to check if it's a palindrome, starting from the last character going back to the first one
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                //reverse the word to be checked and store the result in "myString", initially declared empty
+                myString += word[i];
+            }
+
+            //now check if the word is a palindrome, disregarding the case
+            return word.ToLower().Equals(myString.ToLower());
+
+
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Program.IsPalindrome("Deleveled"));
+        }
+    }
+
+
+}
