32372a85a0a280fc50f1a6f2b996a092698c24e4
Ediezzle <edmore.mandikiyana@gmail.com>
2020-01-27 17:37:18 +0200

    adjust variable names

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -10,10 +10,10 @@ namespace Palindrome
             string myString = "";
 
             //iterate through all the characters in the string we need to check if it's a palindrome, starting from the last character going back to the first one
-            for (int i = word.Length - 1; i >= 0; i--)
+            for (int index = word.Length - 1; index >= 0; index--)
             {
                 //reverse the word to be checked and store the result in "myString", initially declared empty
-                myString += word[i];
+                myString += word[index];
             }
 
             //now check if the word is a palindrome, disregarding the case
